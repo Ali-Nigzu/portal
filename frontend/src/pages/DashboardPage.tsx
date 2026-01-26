db60a9cef600d9e3c3eabb877f26930537b569d0
Ali-Nigzu <c.razakali@gmail.com>
2026-01-26 13:30:06 +0000

    refactor(frontend): move dashboard feature under src/features

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,3 +1,14 @@
-import DashboardV2Page from '../dashboard/v2/pages/DashboardV2Page';
+import React from 'react';
 
-export default DashboardV2Page;
+import DashboardV2Page from '../features/dashboard/v2/pages/DashboardV2Page';
+import { Credentials } from '../types/credentials';
+
+interface DashboardPageProps {
+  credentials: Credentials;
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = ({ credentials }) => (
+  <DashboardV2Page credentials={credentials} />
+);
+
+export default DashboardPage;
